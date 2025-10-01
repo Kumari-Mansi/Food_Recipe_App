e459ed27ef8b6a04590d19490b6c0750d4a855e6
Mansi Kumari <kumarimansi712@gmail.com>
2025-10-01 12:22:27 +0530

    feat: implement main App component with styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Searchbar from "./components/searchbar";
+import RecipeCard from "./components/recepieCard";
+
+const apiurl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = apiurl + query;
+    const res = await fetch(url);
+    const data = await res.json();
+    setRecipes(data.meals || []); // handle null case
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searchRecipes(); // load some recipes initially
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Our Recipe App</h1>
+ 
+      {/* Searchbar */}
+      <Searchbar query={query} setQuery={setQuery} searchRecipes={searchRecipes} />
+
+      <div className="recipes">
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : recipes.length > 0 ? (
+          recipes.map((recipe) => (
+            <RecipeCard key={recipe.idMeal} recipe={recipe} />
+          ))
+        ) : (
+          <p>No Recipe Available!</p>
+        )}
+      </div>
     </div>
   );
 }
